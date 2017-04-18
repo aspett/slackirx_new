c42e11ec6ff6602b4cd3a4991a418711491246b7
Andrew Pett <astuart.pett@gmail.com>
2017-04-18 17:41:57 +1200

    tidy up for github

--- apps/relay/lib/relay/dispatch.ex
+++ apps/relay/lib/relay/dispatch.ex
@@ -5,14 +5,6 @@ defmodule Relay.Dispatch do
     GenServer.start_link(__MODULE__, [], [])
   end
 
-  # @spec dispatch(%{source: atom, type: atom, from: String.t, channel: String.t, message: String.t}) :: :ok | { :error, atom }
-  # def dispatch(event = %{source: :slack, type: :message, channel: "***REMOVED***", from: _from, message: _message}) do
-  #   IO.puts "dispatching"
-  #   IO.inspect(event)
-
-  #   Relay.LocationService.Irc.DispatchHandler.dispatch({:message, event})
-  # end
-
   def determine_destination(from_location = %{pipeline_id: pipeline_id}) do
     pipeline = Relay.Repo.get!(Relay.Location.Pipeline, pipeline_id)
 
@@ -49,21 +41,6 @@ defmodule Relay.Dispatch do
   def dispatch(location = %Relay.Location.Slack{}, dispatch_pid, event) do
     Relay.LocationService.Slack.Supervisor.dispatch(location, dispatch_pid, event)
   end
-
-  # def dispatch(from_location, event = %{source: :slack, type: :message, channel: "***REMOVED***", from: _from, message: _message}) do
-  #   IO.puts "dispatching"
-  #   IO.inspect(event)
-
-  #   Relay.LocationService.Irc.DispatchHandler.dispatch({:message, event})
-  # end
-
-  # def dispatch(event = %{source: :irc, type: :message, from: _from, channel: _channel, message: _message}) do
-  #   send :slack, { :message, event }
-  # end
-
-  # def dispatch(from_location, event = %{source: :irc, type: :message, from: _from, channel: _channel, message: _message}) do
-  #   send :slack, { :message, event }
-  # end
 
   def dispatch(input) do
     IO.inspect(input)
